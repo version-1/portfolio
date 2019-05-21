fe3775bb0ac65153e7d0933e6dbc752e5aef6791
jiro <j.code.public@gmail.com>
2019-05-22 01:05:27 +0900

    update all

--- src/pages/channels/AboutMe.js
+++ src/pages/channels/AboutMe.js
@@ -10,16 +10,12 @@ const Container = styled.div`
   background-color: red;
 `
 
-const IndexPage = () => (
+const AboutMePage = () => (
   <Container>
     <Layout>
-      <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <Link to="/channels/AboutMe">Go to page 2</Link>
+      <SEO title="About Me" />
     </Layout>
   </Container>
 )
 
-export default IndexPage
+export default AboutMePage
