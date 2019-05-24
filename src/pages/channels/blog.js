148337b5d700ef270c60a68e7393cc96a975e068
version-1 <12.05.xxxx@gmail.com>
2019-05-24 22:28:35 +0900

    implment blog page

--- src/pages/channels/blog.js
+++ src/pages/channels/blog.js
@@ -1,19 +1,38 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import styled from 'styled-components'
 
+import Context from 'context'
+import { Body } from 'components/styles'
 import Layout from 'components/templates/Layout'
+import Message from 'components/molecules/Message'
 import Image from 'components/atoms/Image'
 import SEO from 'components/organisms/Seo'
 
-const Container = styled.div`
-  background-color: red;
-`
+class Component extends React.PureComponent {
+  componentDidMount() {
+    const {
+      state: { channels },
+      updatePage,
+    } = this.props.context
+    updatePage(channels[3])
+  }
 
-const BlogPage = () => (
-  <Layout>
-    <SEO title="Blog Page" />
-  </Layout>
+  render() {
+    const { channels } = this.props.context.state
+
+    return (
+      <Layout>
+        <SEO title="blog" />
+        <Body>
+          <Message title="Hello World" body={<></>} />
+        </Body>
+      </Layout>
+    )
+  }
+}
+
+export default props => (
+  <Context.Consumer>
+    {context => <Component props={props} context={context} />}
+  </Context.Consumer>
 )
-
-export default BlogPage
