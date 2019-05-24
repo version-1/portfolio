c9c983e444f350bfce05412f168761b69e95576a
jiro <j.code.public@gmail.com>
2019-05-24 23:02:56 +0900

    fix context

--- src/pages/channels/carrier.js
+++ src/pages/channels/carrier.js
@@ -1,15 +1,45 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import styled from 'styled-components'
 
+import Context from 'context'
+import { Body } from 'components/styles'
 import Layout from 'components/templates/Layout'
+import Message from 'components/molecules/Message'
 import Image from 'components/atoms/Image'
 import SEO from 'components/organisms/Seo'
 
-const AboutMePage = () => (
-  <Layout>
-    <SEO title="About Me" />
-  </Layout>
-)
+class Component extends React.PureComponent {
+  componentDidMount() {
+    const {
+      state: { channels },
+      updatePage,
+    } = this.props.context
 
-export default AboutMePage
+    const path = this.props.props['*']
+    if (path === '/') {
+      updatePage(channels.top)
+    } else {
+      const [key] = path.split('/').slice(-1)
+      updatePage(channels[key])
+    }
+  }
+
+  render() {
+    const { page, channels } = this.props.context.state
+
+    return (
+      <Layout>
+        <SEO title={page.name} />
+        <Body>
+          <Message title="Hello World" body={<></>} />
+        </Body>
+      </Layout>
+    )
+  }
+}
+
+export default props => (
+  <Context.Consumer>
+    {context => <Component props={props} context={context} />}
+  </Context.Consumer>
+)
--- src/pages/channels/me.js
+++ src/pages/channels/me.js
@@ -1,15 +1,41 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import styled from 'styled-components'
 
+import Context from 'context'
+import { Body } from 'components/styles'
 import Layout from 'components/templates/Layout'
+import Message from 'components/molecules/Message'
 import Image from 'components/atoms/Image'
 import SEO from 'components/organisms/Seo'
 
-const ToMePage = () => (
-  <Layout>
-    <SEO title="To jjoo" />
-  </Layout>
-)
+class Component extends React.PureComponent {
+  componentDidMount() {
+    const {
+      state: { dm },
+      updatePage,
+    } = this.props.context
 
-export default ToMePage
+    const path = this.props.props['*']
+    const [key] = path.split('/').slice(-1)
+    updatePage(dm[key])
+  }
+
+  render() {
+    const { page } = this.props.context.state
+
+    return (
+      <Layout>
+        <SEO title={page.name} />
+        <Body>
+          <Message title="Hello World" body={<></>} />
+        </Body>
+      </Layout>
+    )
+  }
+}
+
+export default props => (
+  <Context.Consumer>
+    {context => <Component props={props} context={context} />}
+  </Context.Consumer>
+)
