531abdb2a2464f269c60b026c520050d915329a2
version-1 <12.05.xxxx@gmail.com>
2019-05-27 08:33:50 +0900

    some fix

--- src/services/slack.js
+++ src/services/slack.js
@@ -2,6 +2,7 @@ import slack from 'slack-notify'
 
 const url = process.env.SLACK_WEBHOOK_URL
 const notifier = url ? slack(url) : undefined
+console.log('notifier:', !!notifier)
 
 export const notify = message => {
   const { content } = message
