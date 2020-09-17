e6299aaa9fa970ad147205ade407bacc4736933d
version-1 <12.05.xxxx@gmail.com>
2020-09-17 11:49:35 -0700

    fix fetch feed

--- src/services/rss.ts
+++ src/services/rss.ts
@@ -2,34 +2,13 @@
 import Parser from 'rss-parser'
 const parser = new Parser({
   headers: { 'Access-Control-Allow-Origin': '*' },
+  customFields: {
+    item: ['thumbnailUrl', 'language']
+  }
 })
 
 const domParser: any = typeof DOMParser !== 'undefined' && new DOMParser()
 
 export const fetchRss = async (lang: string) => {
-  let feeds
-  try {
-    feeds = await parser.parseURL(`https://ver-1-0.net/latest.${lang}.xml`)
-  } catch (e) {
-    console.error(e)
-    return e
-  }
-  return feeds
-}
-
-export const fetchThumbnails = async (items: any[]) => {
-  const results = await Promise.all(
-    items.map(async item => {
-      const response = await fetch(item.link)
-      const html = await response.text()
-      const doc = domParser.parseFromString(html, 'text/html')
-      const thumbnail = doc.head.querySelector("meta[property='og:image']")
-        .content
-      return {
-        ...item,
-        thumbnail,
-      }
-    })
-  )
-  return results
+  return await parser.parseURL(`https://ver-1-0.net/latest.${lang}.xml`)
 }
