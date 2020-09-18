61d15c30a4db4bf7ba3219e6297c131e8a58481c
version-1 <12.05.xxxx@gmail.com>
2020-09-18 13:37:41 -0700

    fix hooks

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Provider } from 'context'
-
-export const wrapRootElement = ({ element }) => {
-  return <Provider>{element}</Provider>
-}
--- src/components/templates/Page.tsx
+++ src/components/templates/Page.tsx
@@ -5,6 +5,7 @@ import { Body, P } from 'components/styles'
 import Layout from 'components/templates/Layout'
 import Message from 'components/molecules/Message'
 import SEO from 'components/organisms/Seo'
+import { useApp } from 'hooks/useApp'
 
 const parseContent = (content: string) => {
   return <>{content.split('\n').map(line => (line ? <P>{line}</P> : <p />))}</>
@@ -16,23 +17,29 @@ interface Props {
   messages?: any[]
 }
 
-const Page: React.FC<Props> = ({ children, title, messages }) => {
+const Page: React.FC<Props> = ({ children, title }) => {
+  const { value, Provider } = useApp()
+  if (!value.ready) {
+    return null
+  }
   return (
-    <Layout>
-      <SEO title={title} />
-      <Body>
-        <>
-          {children}
-          {messages?.map((message: any) => (
-            <Message
-              icon={you}
-              title={message.sender}
-              body={parseContent(message.content)}
-            />
-          ))}
-        </>
-      </Body>
-    </Layout>
+    <Provider value={value}>
+      <Layout>
+        <SEO title={title} />
+        <Body>
+          <>
+            {children}
+            {value.messages?.map((message: any) => (
+              <Message
+                icon={you}
+                title={message.sender}
+                body={parseContent(message.content)}
+              />
+            ))}
+          </>
+        </Body>
+      </Layout>
+    </Provider>
   )
 }
 
